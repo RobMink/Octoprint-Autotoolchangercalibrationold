356e614d2eb6f3a8446011e0441f81fbe51797aa
Rob Mink <rob.mink@gmail.com>
2020-10-16 01:26:03 +0000

    Initial Commit

---
+++ NozzleOffsetCalibrationTool.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import octoprint.plugin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__plugin_name__ = "NozzleOffsetCalibrationTool"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "A tool to calibrate nozzle offsets nativly in Octoprint with Klipper"
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = NozzleOffsetCalibrationTool()
